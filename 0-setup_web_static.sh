3096760eb6706eb86c98dd339332d0fe9c18d026
David <1508@holbertonschool.com>
2020-08-17 23:13:41 +0000

    Add apt-get upgrade

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,6 +2,7 @@
 #Bash script that sets up your web servers for the deployment of web_static
 
 sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
