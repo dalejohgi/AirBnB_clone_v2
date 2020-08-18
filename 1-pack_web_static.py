0ab77116d67edaec054696cf12bf06e297d5ee38
David <1508@holbertonschool.com>
2020-08-18 01:45:34 +0000

    Add task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Compress web static folder into a tgz file"""
+
+from datetime import datetime
+from fabric.api import *
+
+
+env.user = "ubuntu"
+
+
+def do_pack():
+    """Compress web static folder into a tgz file"""
+    local("mkdir -p versions")
+    date = datetime.now()
+    now = date.strftime("%Y%m%d%H%M%S")
+    tgz_file = "web_static_{}.tgz".format(now)
+    try:
+        local("tar -cvzf versions/{}\
+              ~/AirBnB_clone_v2/web_static".format(tgz_file))
+        return tgz_file
+    except:
+        return None
