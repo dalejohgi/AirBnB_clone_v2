f5e3cfc00285380e8b3a1e515d95e56bf92e5286
David <1508@holbertonschool.com>
2020-09-02 18:43:08 -0500

    Fix pep8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,14 +10,16 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown_appcontext(self):
-	"""refresh the session"""
-	storage.close()
+        """refresh the session"""
+        storage.close()
+
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-	"""Displays the states list"""
-	states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-	return render_template('7-states_list.html', states=states)
-if __name__ == '__main__':
-	app.run(host='0.0.0.0', port='5000', debug=True)
+        """Displays the states list"""
+        states = sorted(list(storage.all("State").values()),
+                        key=lambda x: x.name)
+        return render_template('7-states_list.html', states=states)
 
+if __name__ == '__main__':
+        app.run(host='0.0.0.0', port='5000', debug=True)
